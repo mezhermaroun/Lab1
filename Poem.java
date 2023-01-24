51dcf04de13cb4823f5900f80282620c032dbe9f
mezhermaroun <maroun.mezher@lau.edu>
2023-01-24 20:20:39 +0200

    Lab 1 contains Poem.java now!

---
+++ Poem.java
@@ -0,0 +1,13 @@
+public class Poem{
+	public static void main (String[] args){
+		System.out.println("Roses are red");
+		System.out.println("Violets are blue");
+		System.out.println("Programming?!");
+		System.out.println("I have total clue!");
+	}
+}
+
+
+
+
+
