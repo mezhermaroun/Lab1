25109a4813f1a10a9e173095da0d378b035078b6
mezhermaroun <maroun.mezher@lau.edu>
2023-01-24 20:45:02 +0200

    My name, major and age

---
+++ Me.java
@@ -0,0 +1,8 @@
+public class Me
+{
+	public static void main (String[] args){
+		System.out.println("Maroun Mezher");
+		System.out.println("18");
+		System.out.println("Mechatronics Engineering");
+	}
+}
