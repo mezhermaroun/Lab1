3309ff95084cf2863e9f6da2a79c9783e6568aec
mezhermaroun <maroun.mezher@lau.edu>
2023-01-24 20:35:34 +0200

    I'm done

---
+++ Problems.java
@@ -0,0 +1,8 @@
+Public class Problem
+{
+	public static void main(String [] args){
+		System.out.println("!!!!!!!!!!!!!!!!!!!");
+		System.out.println("Hello, there are a lot");
+		System.out.print("of problems");
+	}
+}
